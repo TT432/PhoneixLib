ae328c052e515d40da94092069b34a20b425c547
TT432 <2437315224@qq.com>
2021-08-30 12:48:36 +0800

    * gif reg event

--- src/main/java/com/nmmoc7/phoenixlib/client/gif/GIFLoader.java
+++ src/main/java/com/nmmoc7/phoenixlib/client/gif/GIFLoader.java
@@ -1,11 +1,11 @@
 package com.nmmoc7.phoenixlib.client.gif;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.nmmoc7.phoenixlib.event.client.GIFRegisterEvent;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
@@ -29,6 +29,8 @@ public final class GIFLoader {
 
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
-        MinecraftForge.EVENT_BUS.post(new GIFRegisterEvent());
+        event.enqueueWork(() -> {
+            ModLoader.get().postEvent(new GIFRegisterEvent());
+        });
     }
 }
--- src/main/java/com/nmmoc7/phoenixlib/event/client/GIFRegisterEvent.java
+++ src/main/java/com/nmmoc7/phoenixlib/event/client/GIFRegisterEvent.java
@@ -3,11 +3,12 @@ package com.nmmoc7.phoenixlib.event.client;
 import com.nmmoc7.phoenixlib.client.gif.GIFInstance;
 import com.nmmoc7.phoenixlib.client.gif.GIFLoader;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.fml.event.lifecycle.IModBusEvent;
 
 /**
  * run in Render Thread
  */
-public class GIFRegisterEvent extends Event {
+public class GIFRegisterEvent extends Event implements IModBusEvent {
     public void register(GIFInstance gif) {
         GIFLoader.INSTANCE.addGIF(gif);
     }
